77fd4b2e326e4e8409120f429275207f3b563d69
DomingoRodriguez <002854>
2019-10-01 12:04:05 -0400

    code added to test storing information about computer in database called "databasetest1"
    Comment lines to avoid duplicating information in tables.

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-#Here I am going to start writing my final project.
+#main
+
+#Learning about socket library.
+import my_functions
+import socket
+import sqlite3
+
+hostname = socket.gethostname() #this is my computer name
+ip_addr = socket.gethostbyname(hostname) # this is my ip address
+
+print("Your Computer Name is:" + hostname)
+print("Your Computer IP Address is:" + ip_addr)
+
+
+conn = sqlite3.connect('databasetest1.db')
+
+c = conn.cursor()
+
+# c.execute("""CREATE TABLE pc_information (
+#             hostname text,
+#             ipaddress text
+#             )""")
+
+# c.execute("INSERT INTO pc_information VALUES (?, ?)", (hostname, ip_addr))
+#
+# conn.commit()
+
+c.execute("SELECT * FROM pc_information")
+
+print(c.fetchall())
+
+conn.commit()
+
+conn.close()
